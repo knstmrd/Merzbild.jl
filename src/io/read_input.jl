b09284bbe360b25860866ef89e36731db8470c1f
George Oblapenko <kunstmord@kunstmord.com>
2018-11-17 14:41:20 +0300

    reading of base global params

--- src/io/read_input.jl
+++ src/io/read_input.jl
@@ -1,18 +1,41 @@
 using YAML
 
-struct SpeciesInput
+struct SpeciesParams
     name
-    ndens
     fnum
+    nparticles # computed or specified
+    ndens  # either this or nfrac can be specified
     nfrac
-    rho
+    p  # pressure
+    rho  # either this or rhofrac can be specified
     rhofrac
+    df_type  # distribution function
+    df_offset  # offset of distribution function in velocity space
+end
+
+struct GlobalParams
+    particles_file::String
+    interactions_file::String
+
+    fnum::Float64
+    nparticles::Int64
+    ndens::Float64
+    p::Float64
+    rho::Float64
+
+    timestep::Float64
+    num_timesteps::Int64
+    use_single_fnum::Bool  # if fnum is the same for all species, we can simplify collision scheme
 end
 
 function parse_input(filename)
 
     data = YAML.load(open(filename))
-    fnum = data["global fnum"]
-    particles_data_file = data["particles file"]
-    println(particles_data_file, fnum)
+
+    global_params =  GlobalParams(data["particles file"], data["interactions file"], 0.0, 0, 0.0, 0.0, 0.0,
+                                  data["timestep"], data["number of timesteps"], true)
+
+
+    println(data)
+    return global_params
 end
--- src/main.jl
+++ src/main.jl
@@ -1,18 +1,9 @@
 include("io/read_input.jl")
 
 function main()
-    timesteps = 0
-    dt = 0.0
-    species_input_data = SpeciesInput[]
-    particles_filename = ""
-    interactions_filename = ""
-    fnum = 0.0
-    ndens = 0.0
-    rho = 0.0
-    p = 0.0
-
-    parse_input(ARGS[1])
-    println(fnum)
+    species_params = SpeciesParams[]
+    global_params = parse_input(ARGS[1])
+    println(global_params.interactions_file)
 end
 
 main()
