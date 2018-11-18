2ba7b03a81976178fe1e161460c264f93421646f
George Oblapenko <kunstmord@kunstmord.com>
2018-11-18 14:41:34 +0300

    parsing of input file finished

--- src/io/read_input.jl
+++ src/io/read_input.jl
@@ -1,19 +1,20 @@
 using YAML
 
-struct SpeciesParams
-    name
-    fnum
-    nparticles # computed or specified
-    ndens  # either this or nfrac can be specified
-    nfrac
-    p  # pressure
-    rho  # either this or rhofrac can be specified
-    rhofrac
-    df_type  # distribution function
-    df_offset  # offset of distribution function in velocity space
+mutable struct SpeciesParams
+    species::String
+    fnum::Float64
+    nparticles::Int64 # computed or specified
+    ndens::Float64  # either this or nfrac can be specified
+    nfrac::Float64
+    p::Float64  # pressure
+    rho::Float64  # either this or rhofrac can be specified
+    rhofrac::Float64
+    df_type::String  # distribution function
+    df_offset::Array{Float64,1}  # offset of distribution function in velocity space
 end
 
-struct GlobalParams
+mutable struct GlobalParams
+    description::String
     particles_file::String
     interactions_file::String
 
@@ -29,13 +30,32 @@ struct GlobalParams
 end
 
 function parse_input(filename)
-
     data = YAML.load(open(filename))
 
-    global_params =  GlobalParams(data["particles file"], data["interactions file"], 0.0, 0, 0.0, 0.0, 0.0,
+    species_params = SpeciesParams[]
+
+    global_params =  GlobalParams(data["description"], data["particles file"], data["interactions file"], -1.0, -1, -1.0, -1.0, -1.0,
                                   data["timestep"], data["number of timesteps"], true)
 
+    global_params.fnum = get(data, "global fnum", -1.0)
+    global_params.nparticles = get(data, "global nparticles", -1.0)
+    global_params.ndens = get(data, "global ndens", -1.0)
+    global_params.p = get(data, "global p", -1.0)
+    global_params.rho = get(data, "global rho", -1.0)
 
-    println(data)
-    return global_params
+    for k in data["initial conditions"]
+        sp = SpeciesParams(k["species"], -1.0, -1, -1.0, -1.0, -1.0, -1.0, -1.0, k["distribution function"], k["distribution function offset"])
+
+        sp.fnum = get(k, "fnum", -1.0)
+        sp.nparticles = get(k, "nparticles", -1.0)
+        sp.ndens = get(k, "ndens", -1.0)
+        sp.nfrac = get(k, "ndens", -1.0)
+        sp.p = get(k, "p", -1.0)
+        sp.rho = get(k, "rho", -1.0)
+        sp.rhofrac = get(k, "rhofrac", -1.0)
+
+        push!(species_params, sp)
+    end
+
+    return global_params, species_params
 end
