5d603e9df622c322020c41d12b48c3c0ff080b3f
George Oblapenko <kunstmord@kunstmord.com>
2018-11-18 16:26:27 +0300

    basic list of initialization calls added

--- src/main.jl
+++ src/main.jl
@@ -1,9 +1,18 @@
 include("io/read_input.jl")
+include("io/screen_output.jl")
+include("utils/physics_utils.jl")
 
 function main()
-    species_params = SpeciesParams[]
-    global_params = parse_input(ARGS[1])
-    println(global_params.interactions_file)
+    global_params, species_params = parse_input(ARGS[1])
+
+    check_initial_conditions(species_params, global_params)
+
+    output_global_params(global_params)
+
+    # particle_data = load_particle_data(species_params)
+    # interaction_data = load_interaction_data(particle_data)
+    # compute_initial_conditions(species_params, global_params)
+    # particles = create_particles(particle_data, species_params)
 end
 
 main()
