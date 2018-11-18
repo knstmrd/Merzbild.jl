d8b725431a06a620d9d34fd04cbe3baed69fd739
George Oblapenko <kunstmord@kunstmord.com>
2018-11-18 16:25:26 +0300

    code to check and compute initial conditions will go here

---
+++ src/utils/physics_utils.jl
@@ -0,0 +1,7 @@
+function compute_initial_conditions(species_params, global_params)
+
+end
+
+function check_initial_conditions(species_params, global_params)
+    # check initial conditions for consistency
+end
