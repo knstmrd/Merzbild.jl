993cc526183ae1be782d928f682410f0d4b5fdb8
George Oblapenko <kunstmord@kunstmord.com>
2018-12-15 16:41:01 +0300

    particles file renamed to species file, more representative

--- src/io/screen_output.jl
+++ src/io/screen_output.jl
@@ -1,4 +1,4 @@
 function output_global_params(global_params)
     println("Description: ", global_params.description)
-    println("Particles data file: ", global_params.particles_file)
+    println("Species data file: ", global_params.species_file)
 end
