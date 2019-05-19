cfac750121550532af3c4ed54fee743baa010700
George Oblapenko <kunstmord@kunstmord.com>
2019-05-19 18:13:04 -0500

    parameter structs moved to separate file

--- src/io/read_input.jl
+++ src/io/read_input.jl
@@ -1,39 +1,5 @@
 using YAML
 
-mutable struct SpeciesParams
-    species::String
-    fnum::Float64
-    nparticles::Int64 # computed or specified
-
-    ndens::Float64  # either this or nfrac can be specified
-    nfrac::Float64
-    p::Float64  # pressure
-    rho::Float64  # either this or rhofrac can be specified
-    rhofrac::Float64
-
-    T::Float64
-
-    df_type::String  # distribution function
-    df_offset::Array{Float64,1}  # offset of distribution function in velocity space
-end
-
-mutable struct GlobalParams
-    description::String
-    species_file::String
-    interactions_file::String
-
-    fnum::Float64
-    nparticles::Int64
-    ndens::Float64
-    p::Float64
-    rho::Float64
-    T::Float64
-
-    timestep::Float64
-    num_timesteps::Int64
-    use_single_fnum::Bool  # if fnum is the same for all species, we can simplify collision scheme
-end
-
 function parse_input(filename)
     data = YAML.load(open(filename))
 
