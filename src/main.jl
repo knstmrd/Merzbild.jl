22c4644d81d0627593c6c5290292e7a2baa6656a
George Oblapenko <kunstmord@kunstmord.com>
2019-05-19 18:14:05 -0500

    load interaction data and compute simulation parameters

--- src/main.jl
+++ src/main.jl
@@ -1,10 +1,13 @@
+include("data/params.jl")
 include("utils/constants.jl")
 include("io/read_input.jl")
 include("io/screen_output.jl")
 include("utils/physics_utils.jl")
 include("data/species.jl")
+include("data/interactions.jl")
 
 function main()
+    # input: path to input file, path to species data file
     global_params, species_params = parse_input(ARGS[1])
 
     check_initial_conditions(global_params, species_params)
@@ -12,9 +15,14 @@ function main()
     output_global_params(global_params)
 
     species_data = load_species_data(global_params, species_params)
-    # interaction_data = load_interaction_data(particle_data)
-    # compute_initial_conditions(species_params, global_params)
-    # particles = create_particles(particle_data, species_params)
+    interaction_data = load_interaction_data(global_params, species_data)
+    compute_initial_conditions(global_params, species_params, species_data)
+    # particles = create_particles(global_params, species_params, species_data)
+
+    # time counter
+    # collide
+    # compute macroscopic parameters
+    # output
 end
 
 main()
