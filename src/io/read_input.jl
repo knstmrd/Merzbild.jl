a39c0f02c86f5da4498693ce658ce400e7a014c2
George Oblapenko <kunstmord@kunstmord.com>
2019-05-27 17:23:47 -0500

    compute how many species are present in mixture

--- src/io/read_input.jl
+++ src/io/read_input.jl
@@ -5,7 +5,7 @@ function parse_input(filename)
 
     species_params = SpeciesParams[]
 
-    global_params =  GlobalParams(data["description"], data["species file"], data["interactions file"], -1.0, -1, -1.0, -1.0, -1.0, -1.0,
+    global_params =  GlobalParams(data["description"], data["species file"], data["interactions file"], -1.0, -1, -1.0, -1.0, -1.0, -1.0, 0,
                                   data["timestep"], data["number of timesteps"], true)
 
     global_params.fnum = get(data, "global fnum", -1.0)
@@ -26,6 +26,7 @@ function parse_input(filename)
         sp.rho = get(k, "rho", -1.0)
         sp.rhofrac = get(k, "rhofrac", -1.0)
         sp.T = get(k, "T", -1.0)
+        global_params.n_species += 1
 
         push!(species_params, sp)
     end
