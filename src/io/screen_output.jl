6a930b3001087b52c01f2183e40e2506751cb8c8
George Oblapenko <kunstmord@kunstmord.com>
2019-05-27 17:24:34 -0500

    output of basic macroscopic parameters

--- src/io/screen_output.jl
+++ src/io/screen_output.jl
@@ -2,3 +2,14 @@ function output_global_params(global_params)
     println("Description: ", global_params.description)
     println("Species data file: ", global_params.species_file)
 end
+
+function output_per_timestep(global_macro_params, species_macro_params, n_species, t)
+    println("Output at timestep ", t)
+
+    for i in 1:n_species
+        println("Velocity ", species_macro_params[i].vel)
+        println("Temperature ", species_macro_params[i].T)
+        println("Number density ", species_macro_params[i].ndens)
+        println()
+    end
+end
